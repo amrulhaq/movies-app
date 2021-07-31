900be3e9fa98019a31cbe1dba29ccf2deae65ada
Ahmad Qosam Amrul Haq <ahmadqosam@gmail.com>
2021-07-31 14:53:55 +0700

    update base url

--- src/services/omdb/index.js
+++ src/services/omdb/index.js
@@ -3,14 +3,16 @@ import axios from 'axios'
 /**
  * @todo move to config
  */
-const API_KEY = "faf7e5bb"
-
-const omdbRequest = axios.create({
-    baseURL: "http://www.omdbapi.com/",
-    params: {
-        apikey: API_KEY
-    }
-})
+ const OMDB_API_KEY = process.env.OMDB_API_KEY || "faf7e5bb"
+ const OMDB_BASE_URL = process.env.OMDB_BASE_URL || "https://www.omdbapi.com/"
+ 
+ const omdbRequest = axios.create({
+     baseURL: OMDB_BASE_URL,
+     params: {
+         apikey: OMDB_API_KEY
+     }
+ })
+ 
 
 export const getMovieDetail = (movieId) => {
     return omdbRequest.get('', {
